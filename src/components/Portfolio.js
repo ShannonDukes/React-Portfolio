92f57e493304f7380de06007c977e45b4fed1a2e
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-11-13 17:05:19 -0800

    updates to portfolio and images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,85 +6,85 @@ function Portfolio() {
 
     const [projects] = useState([
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'budget.jpg',
+            alt: 'Budget Tracker',
+            title: 'Budget Tracker',
+            description: 'Progressive web app that allows for adding and subtracting money from a budget.',
+            code: '(Express / MySQL / Sequelize / dotenv)',
+            url: 'https://shannondukes.github.io/Budget-Tracker/',
+            git: 'https://github.com/ShannonDukes/Budget-Tracker'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'cmsblog.jpg',
+            alt: 'CMS Blog',
+            title: 'CMS Blog',
+            description: 'Blog site where developers can publish posts and comment on other developer posts.',
+            code: '(Express / MySQL / Sequelize / dotenv)',
+            url: 'https://shannondukes.github.io/CMS-Blog/',
+            git: 'https://github.com/ShannonDukes/CMS-Blog'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'employee-tracker.jpg',
+            alt: 'Employee Tracker',
+            title: 'Employee Tracker',
+            description: 'Command-line Application to view and manage the departments, roles, and employees of a company in order to organize and plan a business.',
+            code: '(Inquirer, Node, MySQL 2, Jest)',
+            url: 'https://shannondukes.github.io/Employee-Tracker/',
+            git: 'https://github.com/ShannonDukes/Employee-Tracker'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'fab-four.jpg',
+            alt: 'Adventure Time',
+            title: 'Adventure Time',
+            description: 'A search engine for events and dining that provides information for decision making and adventure planning.',
+            code: '(JavaScript, CSS, HTML, Tailwind, Google Places Library, Ticketmaster API, Weather API, Location API)',
+            url: 'https://mjos7.github.io/adventure-time/',
+            git: 'https://github.com/mjos7/adventure-time'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'finished-product.jpg',
+            alt: 'Team Profile Generator',
+            title: 'Team Profile Generator',
+            description: 'This is a Node.js application that will generate a team profile page based on user input for companies with teams of employees.',
+            code: '(Inquirer, Jest)',
+            url: 'https://shannondukes.github.io/Team-Profile-Generator/',
+            git: 'https://github.com/ShannonDukes/Team-Profile-Generator'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'hero-bg.jpg',
+            alt: 'Run Buddy',
+            title: 'Run Buddy',
+            description: 'First edition of the Run Buddy site to learn HTML and CSS.',
+            code: '(HTML / CSS)',
+            url: 'https://shannondukes.github.io/run-buddy/',
+            git: 'https://github.com/ShannonDukes/run-buddy'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'notesaver1.jpg',
+            alt: 'Note Saver',
+            title: 'Note Saver',
+            description: 'App that allows a user to add notes or tasks. Notes can be edited or removed as needed.',
+            code: '(Express / Jest)',
+            url: 'https://shannondukes.github.io/Note-Saver/',
+            git: 'https://github.com/ShannonDukes/Note-Saver'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'screenshot.jpg',
+            alt: 'Book Search',
+            title: 'Book Search',
+            description: 'This is an application that allows you to search for books online and saved them to list of your saved books for later review as well as remove the books you no longer wish to track.',
+            code: '(Express, React, MongoDB, Mongoose, Apollo, GraphQL, Node)',
+            url: 'https://shannondukes.github.io/Book-Search/',
+            git: 'https://github.com/ShannonDukes/Book-Search'
         },
         {
-            image: '',
-            alt: '',
-            title: '',
-            description: '',
-            code: '',
-            url: '',
-            git: ''
+            image: 'work-day-scheduler-img.jpg',
+            alt: 'Work Day Scheduler',
+            title: 'Work Day Scheduler',
+            description: 'This calendar app for scheduling your work day was designed to enhance my skills using 3rd party APIs.',
+            code: '(HTML, CSS, Bootstrap, jQuery, Moment.js)',
+            url: 'https://shannondukes.github.io/Work-Day-Scheduler/',
+            git: 'https://github.com/ShannonDukes/Work-Day-Scheduler'
         },
     ])
 
